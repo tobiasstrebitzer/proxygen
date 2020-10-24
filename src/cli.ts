aa81afed5f6d44a5f7be391d00511f8c39a8eeea
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2020-10-24 15:12:28 +0800

    Expand home dir in config path

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 
 import { existsSync, readFileSync } from 'fs-extra'
+import { homedir } from 'os'
+import { resolve } from 'path'
 import { Proxygen, ProxygenConfig } from './index'
 
 async function main() {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
   const [arg] = process.argv.slice(-1)
-  const configPath = /\.json$/.test(arg) ? arg : 'proxygen.json'
+  const configPath = resolve((/\.json$/.test(arg) ? arg : 'proxygen.json').replace('~', homedir()))
   if (!existsSync(configPath)) { throw new Error('Unable to detect path to proxygen config') }
   const config: ProxygenConfig = JSON.parse(readFileSync(configPath, 'utf8'))
   const proxygen = new Proxygen(config)
